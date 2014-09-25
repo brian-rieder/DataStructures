8ddb51639c28c06201f4fc924837029fdfec2273
Brian Rieder <brieder@purdue.edu>
2014-09-25 13:13:44 -0400

    Sequence 2 working, began Sequence 1

--- Project1/sorting.c
+++ Project1/sorting.c
@@ -92,7 +92,10 @@ int Save_File(char * Filename, long * Array, int Size)
     sprintf(strbuffer,"%ld\n", Array[i]);
     fputs(strbuffer, fp);
   }
-
+  
+  //Clean up memory and return success
+  free(strbuffer);
+  fclose(fp);
   return 1;
 }
 
@@ -139,7 +142,7 @@ Output:
   NONE -- void function
 -----------------------------------------------------------------------------*/
 
- void Improved_Bubble_Sort(long * Array, int Size, 
+void Improved_Bubble_Sort(long * Array, int Size, 
 			  double * N_Comp, double * N_Move)
 {
 }
@@ -147,41 +150,127 @@ Output:
 //-----------------------------------------------------------------------------
 
 /*--------------------------------------------------------------------Save Seq1
-The function should generate Seq1 that is identical to the one used in 
-your sorting code, and write Seq1 to an external file specified by 
-Filename. N is the size of the array to be sorted, NOT the size of Seq1.
-You implement this function to allow the grader to check whether you 
-can generate the right Seq1. 
+  The function should generate Seq1 that is identical to the one used in 
+  your sorting code, and write Seq1 to an external file specified by 
+  Filename. N is the size of the array to be sorted, NOT the size of Seq1.
+  You implement this function to allow the grader to check whether you 
+  can generate the right Seq1. 
+
+  {2^p′3^q′, ..., 2^p3^q, ..., 16, 12, 9, 8, 6, 4, 3, 2, 1}, where 2^p′3^q′ is 
+  the largest integer smaller than N (the size of the array to be sorted). 
 
 Input:
-
+  char * Filename -- the name of a file in the current directory as a
+    string.
+  int N -- the size of the array to be sorted.
 
 Output:
-
+  NONE -- void function
 -----------------------------------------------------------------------------*/
+
+// THOUGHTS PRIOR TO STOPPING WORK
+// Generate all numbers in a function -- sort in array?
+// qsort the numbers descending
+// output array to file
+
+
+//Puts one integer to a file opened for writing.
+void intToFile(FILE * fp, int n)
+{
+  char strbuffer[6];
+  sprintf(strbuffer, "%d\n", n);
+  fputs(strbuffer, fp);
+}
+
+//Generates the next value in the sequence
+int gen1val (int num)
+{
+
+}
+
+void seq1_Helper(fp, N, -1)
+{
+
+}
 
 void Save_Seq1 (char *Filename, int N)
 {
+  //Open to WRITE to file
+  FILE * fp = fopen(Filename,"w");
+
+  //Use helper function to generate and write to file
+  seq1_Helper(fp, N, -1);
+
+  //Clean up memory
+  fclose(fp);
 }
 
 //-----------------------------------------------------------------------------
 
 /*--------------------------------------------------------------------Save Seq2
-The function should generate Seq2 that is identical to the one used in
-your sorting code, and write Seq1 to an external file specified by 
-Filename. N is the size of the array to be sorted, NOT the size of Seq1.
-You implement this function to allow the grader to check whether you 
-can generate the right Seq2.
-
+  The function should generate Seq2 that is identical to the one used in
+  your sorting code, and write Seq1 to an external file specified by 
+  Filename. N is the size of the array to be sorted, NOT the size of Seq1.
+  You implement this function to allow the grader to check whether you 
+  can generate the right Seq2.
+  
+  {N/1.3, N/(1.3)2, N/(1.3)3, …, 1}, where N is the size of the array to be sorted. 
+  In other words, the next value in the gap sequence is the previous gap divided
+  by 1.3. However, if any value in this sequence becomes 9 or 10, it is replaced 
+  by a value of 11. Note that since the gap value has to be an integer, the floor 
+  of the gap value computed will need to be taken. 
+  
 Input:
-
-
+  char * Filename -- the name of a file in the current directory as a
+    string.
+  int N -- the size of the array to be sorted.
+  
 Output:
+  NONE -- void function
+------------------------------------------------------------------------------*/
 
------------------------------------------------------------------------------*/
+//Generates the next value in the sequence
+int gen2val (int num)
+{
+  //Divide value by 1.3
+  int ret = (int)((double)num/1.3);
+
+  //If any value is becomes 9 or 10, it is replaced by 11.
+  if(ret == 9 || ret == 10)
+    ret = 11;
+
+  //Return adjusted value
+  return ret;
+}
+
+void seq2_Helper (FILE * fp, int N, int prevval)
+{
+  //Base Case: Terminate if previous value was 1
+  if(prevval == 1)
+    return;
+  //Initial Case: First value in sequence is N/1.3
+  if(prevval == -1) {
+    prevval = gen2val(N);
+    intToFile(fp, prevval);
+    seq2_Helper(fp, N, prevval);
+    return;
+  }
+  //Recursive Case: Next value is the previous divided by 1.3
+  prevval = gen2val(prevval);
+  intToFile(fp, prevval);
+  seq2_Helper(fp, N, prevval);
+}
 
 void Save_Seq2 (char *Filename, int N)
 {
+  //Open to WRITE to file
+  FILE * fp = fopen(Filename,"w");
+
+  //Use helper function to generate and write to file
+  seq2_Helper(fp, N, -1);
+
+  //Clean up memory
+  fclose(fp);
 }
 
 //-----------------------------------------------------------------------------
@@ -189,6 +278,7 @@ void Save_Seq2 (char *Filename, int N)
 //-----------------------------------------------TEMPORARY MAIN, PLEASE REMOVE!
 int main(int argc, char * * argv)
 {
+  /*
   int z = 0;
   long * output = Load_File("sample-input.txt", &z);
   //printf("Length of file: %d\n",z);
@@ -197,6 +287,9 @@ int main(int argc, char * * argv)
     //printf("%ld\n",output[i]);
   }
   Save_File("empty_file.txt", output, z);
+  */
+
+  Save_Seq2 ("seq2_output.txt", 50);
 
   return 0;
 }
